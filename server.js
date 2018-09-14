f572481cda8d9d478a9679912b3a101a58ba4d2e
DanFein <danfeinstat@gmail.com>
2018-09-13 17:51:22 -0700

    updated server for deployment

--- server.js
+++ server.js
@@ -20,6 +20,17 @@ dotenv.config({ path: ".env" });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //Contact Route
 app.post("/api/message", function(req, res) {
   let newMessage = req.body;
